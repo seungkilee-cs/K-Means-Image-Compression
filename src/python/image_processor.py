247c3798859451515e0350ec1052b925bf2712ce
Seung Ki Lee <seungkilee.cs@gmail.com>
2025-05-27 10:28:11 +0900

    scaffolding based on the understading

--- src/python/image_processor.py
+++ src/python/image_processor.py
@@ -1,1 +1,33 @@
 # driver for image processing
+import numpy as np
+from PIL import Image
+
+
+def load_image(path: str) -> Image.Image:
+    """Load image and return as numpy array"""
+    pass
+
+
+def save_image(image: Image.Image, path: str) -> None:
+    """Save PIL Image to file path"""
+    pass
+
+
+def image_to_array(image: Image.Image) -> np.ndarray:
+    """Convert PIL Image to np array"""
+    pass
+
+
+def array_to_image(arr: np.ndarray) -> Image.Image:
+    """Convert np array to PIL Image"""
+    pass
+
+
+def flatten_image_array(arr: np.ndarray) -> np.ndarray:
+    """flatten 3d image array of width, height and color to 2d array of N_pixels, Color"""
+    pass
+
+
+def unflatten_image_array(arr: np.ndarray) -> np.ndarray:
+    """unflatten 2d array of N_pixels, Color to width, height and color"""
+    pass
--- src/python/lloyd.py
+++ src/python/lloyd.py
@@ -1,1 +1,35 @@
 # lloyd's algorithm implementation
+import numpy as np
+
+
+def initialize_centroids(data: np.ndarray, k: int) -> np.ndarray:
+    """Randomly select k data points as initial centroids"""
+    pass
+
+
+def assign_clusters(data: np.ndarray, centroids: np.ndarray) -> np.ndarray:
+    """Assign each data point to nearest centroids and return array of cluster indices"""
+    pass
+
+
+def update_centroids(data: np.ndarray, labels: np.ndarray, k: int) -> np.ndarray:
+    """Update centroids as the mean of assigned data points"""
+    pass
+
+
+def has_converged(
+    old_centroid: np.ndarray, new_centroid: np.ndarray, tolerance: float = 1e-4
+) -> bool:
+    """check if the cntroids have converged within the set tolerance"""
+    return True
+
+
+def kmeans(
+    data: np.ndarray, k: int, max_iter: int = 100, tolerance: float = 1e-4
+) -> tuple[np.ndarray, np.ndarray]:
+    """
+    run lloyd's k means algorithm, returning tuple of centroids and labels
+        centroids: np.ndarray of shape (k, features)
+        labels: np.ndarray of shape (n_samples,)
+    """
+    return (None, None)
